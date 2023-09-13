2c6e1520fa813763e5ccbf209eff1819eed2843b
nockchun <nockchun@gmail.com>
2023-09-13 17:03:58 +0900

    upgrade time to image abstract class

--- danbi/extends/bitensorflow/tfrecord_ext.py
+++ danbi/extends/bitensorflow/tfrecord_ext.py
@@ -10,19 +10,26 @@ def getWriter(file_path: str, is_zip: bool = True) -> tf.io.TFRecordWriter:
     else:
         return tf.io.TFRecordWriter(file_path)
 
-def storeTFRecord(writer: Callable, datas: Dict[str, np.array], is_zip: bool = True):
-    for data in zip(*datas.values()):
-        feature = {}
-        for idx, name in enumerate(datas.keys()):
-            feature[name] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[data[idx].tobytes()]))
-        example = tf.train.Example(features=tf.train.Features(feature=feature))
-        writer.write(example.SerializeToString())
+def storeTFRecord(writer: Callable, data: Dict[str, np.array], is_zip: bool = True):
+    # for data in zip(*datas.values()):
+    #     feature = {}
+    #     for idx, name in enumerate(datas.keys()):
+    #         feature[name] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[data[idx].tobytes()]))
+    #     example = tf.train.Example(features=tf.train.Features(feature=feature))
+    #     writer.write(example.SerializeToString())
+
+    feature = {}
+    for idx, name in enumerate(data.keys()):
+        feature[name] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[data[name].tobytes()]))
+    example = tf.train.Example(features=tf.train.Features(feature=feature))
+    writer.write(example.SerializeToString())
 
 def getTFRecordDecoder(datas: Dict[str, tf.DType], shape_in: Dict[str, Tuple[int]], shape_out: Dict[str, Tuple[int]]) -> Callable:
+    feature_shape = {}
+    for name in datas.keys():
+        feature_shape[name] = tf.io.FixedLenFeature([], tf.string)
+        
     def tfrecordParseAndDecode(dataset):
-        feature_shape = {}
-        for name in datas.keys():
-            feature_shape[name] = tf.io.FixedLenFeature([], tf.string)
         features = tf.io.parse_single_example(dataset, features=feature_shape)
         
         ins = {}
