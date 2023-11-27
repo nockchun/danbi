23ce81696e014e936e5b5ca9ab668390e8889189
nockchun <nockchun@gmail.com>
2023-11-27 16:10:33 +0900

    upgrade function

--- danbi/dataprocess/signal.py
+++ danbi/dataprocess/signal.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
+from typing import List, Union
 
-def genSignData(freq: int = 1, x_range: int = 1000, y_min: int = 0, y_max: int = 65536, sample: int = 1, figsize: tuple = (10, 4)):
+
+def genSignData(freq: Union[int, List[int]], x_range: int = 1000, y_min: int = 0, y_max: int = 65536, sample: int = 1, figsize: tuple = None):
     y_range = y_max - y_min
-    x_values = range(x_range * sample)
-    y_values = [(math.sin(2 * math.pi * freq * x / x_range) * (y_range / 2) + ((y_max + y_min) / 2)) for x in x_values]
-    if figsize is not None or figsize == (0, 0):
-        plt.figure(figsize=(10, 4))
-        plt.plot(x_values, y_values)
+    x_value = np.array(range(x_range * sample))
+    y_value = None
+
+    if isinstance(freq, int):
+        freq = [freq]
+    for f in freq:
+        y_temp = np.array([(math.sin(2 * math.pi * f * x / x_range) * (y_range / 2) + ((y_max + y_min) / 2)) for x in x_value])
+        y_value = y_temp if y_value is None else y_value + np.array(y_temp)
+    
+    min_val = np.min(y_value)
+    max_val = np.max(y_value)
+    y_scaled = y_min + (y_value - min_val) * (y_max - y_min) / (max_val - min_val)
+
+    if figsize is not None:
+        plt.figure(figsize=figsize)
+        plt.plot(x_value, y_scaled)
         plt.title(f'Sign Wave with Frequency {freq} and Y Range {y_min} to {y_max}')
         plt.xlabel('x')
         plt.ylabel(f'Value ({y_min} to {y_max})')
         plt.grid(True)
         plt.show()
-    return list(x_values), y_values
+    
+    return x_value, y_scaled
+
