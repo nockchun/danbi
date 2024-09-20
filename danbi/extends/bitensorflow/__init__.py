ea0782711ddaa686e9e41576cdadcbc1a13675f1
nockchun <nockchun@gmail.com>
2024-09-20 13:29:01 +0900

    add function

--- danbi/extends/bitensorflow/__init__.py
+++ danbi/extends/bitensorflow/__init__.py
@@ -1,3 +1,3 @@
-from .tfrecord_ext import getWriter, storeTFRecord, getTFRecordDecoder, restoreTFRecord
+from .tfrecord_ext import getWriter, storeTFRecord, getTFRecordDecoder, restoreTFRecord, selectTFRecordIndexData
 from .visualization import showLearningHistory
 from .layers import *
--- danbi/extends/bitensorflow/tfrecord_ext.py
+++ danbi/extends/bitensorflow/tfrecord_ext.py
@@ -53,3 +53,40 @@ def restoreTFRecord(files: List[str], decoder: Callable, is_zip: bool = True) ->
     rds = rds.map(decoder)
     
     return rds
+
+def selectTFRecordIndexData(tfrecord, name, indices, axis=-1):
+    """
+    특정 키의 텐서에서 지정한 인덱스만 선택하여 새로운 데이터셋을 생성합니다.
+    
+    Args:
+        tfrecord (tf.data.Dataset): 입력 데이터셋.
+        name (str): 선택할 텐서가 저장된 딕셔너리 내의 키 이름.
+        indices (list or tuple): 선택할 인덱스의 리스트.
+        axis (int): 인덱스를 선택할 차원.
+    
+    Returns:
+        tf.data.Dataset: 선택된 인덱스로 수정된 새로운 데이터셋.
+    """
+    # 인덱스를 텐서로 변환하여 map 함수 내에서 사용 가능하게 함
+    indices_tensor = tf.constant(indices, dtype=tf.int32)
+    
+    def map_fn(*elements):
+        """
+        데이터셋의 각 요소에 적용될 매핑 함수.
+        요소는 여러 개의 딕셔너리로 구성된 튜플일 수 있음.
+        """
+        new_elements = []
+        for element in elements:
+            new_dict = {}
+            for k, v in element.items():
+                if k == name:
+                    # 지정한 키가 존재하면, 해당 텐서에서 지정한 축과 인덱스를 사용하여 선택
+                    selected = tf.gather(v, indices_tensor, axis=axis)
+                    new_dict[k] = selected
+                else:
+                    # 다른 키는 그대로 유지
+                    new_dict[k] = v
+            new_elements.append(new_dict)
+        return tuple(new_elements)
+    
+    return tfrecord.map(map_fn)
